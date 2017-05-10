4055476c4d7932011672e2983bb28427d613a5ca
ericMcAllister <ericster@hotmail.co.uk>
2017-05-11 00:34:40 +0100

    mostly style changes

--- darktennis/GameController.swift
+++ darktennis/GameController.swift
@@ -12,14 +12,20 @@ import CoreMotion
 
 class GameController: UIViewController {
 
+    @IBOutlet var scoreLabel: UILabel!
     @IBOutlet var xrotLabel: UILabel!
     @IBOutlet var yrotLabel: UILabel!
     @IBOutlet var zrotLabel: UILabel!
     @IBOutlet var ball: UIImageView!
+    @IBOutlet var paddle: UIButton!
+    var score = 0;
     var side = 0
     var pos = 0
     var i = 40
+    var vx = 0.0
     var vy = 2.0
+    var vxabs = 0.0
+    var vyabs = 2.0
     let screenSize = UIScreen.main.bounds
     
     let motionManager = CMMotionManager()
@@ -37,17 +43,20 @@ class GameController: UIViewController {
     }
     
     func update() {
-        if(i < 20){
-            vy = 2
+        vyabs = abs(vy)
+        if(i < -Int(screenSize.height)){
+            vy = vyabs
         }
         print(i)
         i = i + Int(vy)
         print("butts: \(i)")
         if(i > Int(screenSize.height)){
-         i = 0
+            score = 0
+            self.scoreLabel.text = "\(score)"
+            i = 0
         }
         
-        ball.frame = CGRect(x: ball.frame.origin.x, y: CGFloat(i), width: ball.frame.size.width, height: ball.frame.size.height)
+        ball.frame = CGRect(x: screenSize.width/2, y: CGFloat(i), width: ball.frame.size.width, height: ball.frame.size.height)
         
 //        if let accelerometerData = motionManager.accelerometerData {
 //            print(accelerometerData)
@@ -68,18 +77,26 @@ class GameController: UIViewController {
             let xnum = gyroData!.rotationRate.x
             
             if xnum < -2.0{
-                if i >= Int(screenSize.height) - 50{
+                if i > Int(screenSize.height)/2 - 50 && i < Int(screenSize.height)/2 + 50 && vy > 0{
+                    updateScore()
                     vy = xnum
-                    ball.frame = CGRect(x: 10, y: CGFloat(i), width: ball.frame.size.width, height: ball.frame.size.height)
+                    ball.frame = CGRect(x: screenSize.width/2, y: CGFloat(i), width: ball.frame.size.width, height: ball.frame.size.height)
                 }
                 
 //                self.view.backgroundColor = UIColor.red
+                
+                
             } else if xnum < 2.0 {
+                if i > Int(screenSize.height){
+                    score = -1
+                    updateScore()
+                }
 //                self.view.backgroundColor = UIColor.white
             } else{
-                if i > Int(screenSize.height) - 50{
+                if i > Int(screenSize.height)/2 - 50 && i < Int(screenSize.height)/2 + 50 && vy > 0{
+                    updateScore()
                     vy = -1*xnum
-                    ball.frame = CGRect(x: CGFloat(Int(screenSize.width) - 50), y: CGFloat(i), width: ball.frame.size.width, height: ball.frame.size.height)
+                    ball.frame = CGRect(x: screenSize.width/2, y: CGFloat(i), width: ball.frame.size.width, height: ball.frame.size.height)
                 }
                 
 //                self.view.backgroundColor = UIColor.blue
@@ -88,11 +105,18 @@ class GameController: UIViewController {
         }
         
         
+        
+        
 //        if let magnetometerData = motionManager.magnetometerData {
 //            print(magnetometerData)
 //        }
 //        if let deviceMotion = motionManager.deviceMotion {
 //            print(deviceMotion)
 //        }
+    }
+    
+    func updateScore() {
+        score = score + 1
+        self.scoreLabel.text = "\(score)"
     }
 }
