64f0963f68fa20d57fba759cdd3e4208d2ff719c
ericMcAllister <ericster@hotmail.co.uk>
2017-05-17 16:01:09 +0100

    added basic noises to gameController

--- darktennis/GameController.swift
+++ darktennis/GameController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreMotion
+import AVFoundation
+import AudioToolbox
 
 
 class GameController: UIViewController {
@@ -28,8 +30,10 @@ class GameController: UIViewController {
     var vyabs = 2.0
     let screenSize = UIScreen.main.bounds
     
+    var wizardAsShitSoundEffect: AVAudioPlayer?
     let motionManager = CMMotionManager()
     var timer: Timer!
+    var END_TIME = 1.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,13 +46,48 @@ class GameController: UIViewController {
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
     }
     
+    func playAudio(sound: String) {
+        var url = Bundle.main.url(forResource: "bounce-left", withExtension: "mp3")!
+        var start = 0.01
+        if(sound == "bounce"){
+            url = Bundle.main.url(forResource: "bounce-left", withExtension: "mp3")!
+            start = 0.5
+        } else{
+            url = Bundle.main.url(forResource: "swoosh", withExtension: "mp3")!
+            start = 0.0
+        }
+        
+        do {
+            wizardAsShitSoundEffect = try AVAudioPlayer(contentsOf: url)
+            guard let wizSound = wizardAsShitSoundEffect else { return }
+            let randomNum:UInt32 = arc4random_uniform(0)
+            wizSound.prepareToPlay()
+            wizSound.currentTime = Double(randomNum)*0.35 + start
+            wizSound.play()
+            
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameController.endSound), userInfo: nil, repeats: true)
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    func endSound() {
+        if (wizardAsShitSoundEffect?.currentTime)! >= END_TIME {
+            wizardAsShitSoundEffect?.stop()
+            timer.invalidate()
+        }
+    }
+    
     func update() {
         vyabs = abs(vy)
         if(i < -Int(screenSize.height)){
             vy = vyabs
+            playAudio(sound: "swoosh")
         }
         print(i)
-        i = i + Int(vy)
+        i = i + 2*Int(vy)
         print("butts: \(i)")
         if(i > Int(screenSize.height)){
             score = 0
@@ -76,16 +115,19 @@ class GameController: UIViewController {
             
             let xnum = gyroData!.rotationRate.x
             
+            if(i > 0 - Int(screenSize.height)/2 - 40 && i < 0 - Int(screenSize.height)/2 + 40){
+                playAudio(sound: "bounce")
+            }
+            
             if xnum < -2.0{
                 if i > Int(screenSize.height)/2 - 50 && i < Int(screenSize.height)/2 + 50 && vy > 0{
                     updateScore()
                     vy = xnum
+                    playAudio(sound: "swoosh")
                     ball.frame = CGRect(x: screenSize.width/2, y: CGFloat(i), width: ball.frame.size.width, height: ball.frame.size.height)
                 }
                 
 //                self.view.backgroundColor = UIColor.red
-                
-                
             } else if xnum < 2.0 {
                 if i > Int(screenSize.height){
                     score = -1
@@ -96,6 +138,7 @@ class GameController: UIViewController {
                 if i > Int(screenSize.height)/2 - 50 && i < Int(screenSize.height)/2 + 50 && vy > 0{
                     updateScore()
                     vy = -1*xnum
+                    playAudio(sound: "swoosh")
                     ball.frame = CGRect(x: screenSize.width/2, y: CGFloat(i), width: ball.frame.size.width, height: ball.frame.size.height)
                 }
                 
